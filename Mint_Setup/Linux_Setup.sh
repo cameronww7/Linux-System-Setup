af520f21f3f1d45c1e6b8a2ccc82a517b2a04235
cameronww7 <cameronww7@gmail.com>
2021-03-14 19:13:57 -0700

    Updating Linux Setup

--- Mint_Setup/Linux_Setup.sh
+++ Mint_Setup/Linux_Setup.sh
@@ -1,7 +1,7 @@
 # Note
 # ---------------------------------------
 # Make sure file has needed perms
-# chmod +x Kali_Setup.sh
+# chmod +x Linux_Setup.sh
 
 echo "\n\n\n Update + Install Basics"
 
@@ -16,24 +16,6 @@ sudo apt-get clean -y
 sudo apt-get full-upgrade -y
 sudo apt-get autoremove -y
 
-# Add search cache
-sudo apt-get-cache search kali-
-
-# Cloning PipMyKali - https://github.com/Dewalt-arch/pimpmykali
-# ---------------------------------------
-echo "\n\n\n Cloning - PimyMyKali \n"
-sudo git clone https://github.com/Dewalt-arch/pimpmykali /opt/pimpmykali/
-sudo chmod +x /opt/pimpmykali/pimpmykali.sh
-cd /opt/pimpmykali/
-#sudo ./pimpmykali.sh
-
-# Cloning HotWax - https://github.com/BrashEndeavours/hotwax.git
-# ---------------------------------------
-echo "\n\n\n Cloning - HotWax \n"
-sudo git clone https://github.com/BrashEndeavours/hotwax.git /opt/hotwax/
-#cd /opt/hotwax/
-#sudo apt install -y git ansible
-#sudo ansible-playbook playbook.yml -K
 
 # Install Dev Tools
 # ---------------------------------------
@@ -81,52 +63,15 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/v
 sudo apt-get install -y code 
 
 
-# Tool Install
+# System Tool Install
 # ---------------------------------------
-echo "\n\n\n Installing - Tools \n"
+echo "\n\n\n Installing - System Tools \n"
 sudo apt-get install -y gedit
-sudo apt-get install -y gobuster
-sudo apt-get install -y sslscan
-sudo apt-get install -y nikto
-sudo apt-get install -y joomscan
-sudo apt-get install -y wpscan
-sudo apt-get install -y smbmap
-sudo apt-get install -y enum4linux
-sudo apt-get install -y dnsrecon
-sudo apt-get install -y odat
-sudo apt-get install -y seclists
-sudo apt-get install -y ffuf
-sudo apt-get install -y curl 
-sudo apt-get install -y nbtscan 
-sudo apt-get install -y nmap 
-sudo apt-get install -y onesixtyone 
-sudo apt-get install -y oscanner 
-sudo apt-get install -y smbclient 
-sudo apt-get install -y snmp 
-sudo apt-get install -y sipvicious 
-sudo apt-get install -y tnscmd10g 
-sudo apt-get install -y whatweb 
-sudo apt-get install -y khtmltopdf 
-sudo apt-get install -y smtp-user-enum 
-sudo apt-get install -y nishang
+sudo apt-get install -y tree
+sudo apt-get install -y htop
+sudo apt-get install -y gedit
+sudo apt-get install -y glances
 
-# Install droopescan - https://github.com/droope/droopescan
-sudo pip install droopescan 
-
-# Install Impacket - https://github.com/SecureAuthCorp/impacket
-sudo git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket/
-cd /opt/impacket/
-pip install /opt/impacket/.
-
-# Insatll AutoRecon - https://github.com/Tib3rius/AutoRecon#installation
-echo "\n\n\n Installing - AutoRecon \n"
-sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git 
-
-# install nmapAutomator - https://github.com/21y4d/nmapAutomator
-echo "\n\n\n Installing - nmapAutomator \n"
-cd /opt/
-sudo git clone https://github.com/21y4d/nmapAutomator.git /opt/nmapAutomator/
-sudo ln -s /opt/nmapAutomator/nmapAutomator.sh /usr/local/bin/
 
 # Install Chrome
 echo "\n\n\n Installing - Chrome \n"
