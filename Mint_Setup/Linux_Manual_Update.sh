133f0b16022ee826a6339f9376dc52ca7517ecdb
cameronww7 <cameronww7@gmail.com>
2021-03-14 20:00:24 -0700

    Added A Linux manual Updater

---
+++ Mint_Setup/Linux_Manual_Update.sh
@@ -0,0 +1,43 @@
+# Note
+# ---------------------------------------
+# Make sure file has needed perms
+# chmod +x Kali_Setup.sh
+
+echo "\n\n\n Update + Install Basics"
+
+# Update the System
+# ---------------------------------------
+sudo apt-get update
+sudo apt-get clean -y
+sudo apt-get full-upgrade -y
+sudo apt-get autoremove -y
+
+# pip Tool Install
+# ---------------------------------------
+echo "\n\n\n Updating - termcolor \n"
+sudo pip install termcolor -U
+
+# Install droopescan - https://github.com/sc0tfree/updog
+echo "\n\n\n Updating - updog \n"
+sudo pip install updog -U
+
+# Install droopescan - https://github.com/giampaolo/psutil/blob/master/INSTALL.rst
+echo "\n\n\n Updating - psutil \n"
+sudo pip3 install psutil -U
+
+# Install Terminal Tools + Customization
+# ---------------------------------------
+echo "\n\n\n Updating - Terminal Tools + Customization \n"
+cd /opt/Terminal-Customization/
+sudo git fetch -A
+sudo git pull
+
+# Setup my File Strucutres 
+sudo mkdir ~/Hacking
+sudo chmod -R 777 ~/Hacking  
+
+sudo chmod -R 777 /opt 
+
+# Reboot Prompt
+# ---------------------------------------
+echo "\n\n\n Finished - REBOOT \n"
