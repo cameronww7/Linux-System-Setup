03b777d783912c6303d24bb0a9036715e8a0eda9
cameronww7 <cameronww7@gmail.com>
2021-03-24 19:09:38 -0700

    More fixes

--- Linux_Setup/Linux_Manual_Update.sh
+++ Linux_Setup/Linux_Manual_Update.sh
@@ -17,10 +17,6 @@ sudo apt-get autoremove -y
 echo "\n\n\n Updating - termcolor \n"
 sudo pip install termcolor -U
 
-# Install droopescan - https://github.com/sc0tfree/updog
-echo "\n\n\n Updating - updog \n"
-sudo pip install updog -U
-
 # Install droopescan - https://github.com/giampaolo/psutil/blob/master/INSTALL.rst
 echo "\n\n\n Updating - psutil \n"
 sudo pip3 install psutil -U
@@ -32,7 +28,7 @@ cd /opt/Terminal-Customization/
 sudo git fetch -A
 sudo git pull
 
-sudo ./opt/Terminal-Customization/Mint/manual_update.sh
+sudo ./opt/Terminal-Customization/Linux/manual_update.sh
 
 # Setup my File Strucutres 
 sudo mkdir ~/Hacking
