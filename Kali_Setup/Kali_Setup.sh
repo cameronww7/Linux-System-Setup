262f0221873c67ed3502fd77751b33f1e4847eb9
cameronww7 <cameronww7@gmail.com>
2021-03-24 19:44:03 -0700

    Testing found I need to add an update

--- Kali_Setup/Kali_Setup.sh
+++ Kali_Setup/Kali_Setup.sh
@@ -82,6 +82,7 @@ echo "\n\n\n Installing - VSCode \n"
 sudo apt-get install -y software-properties-common apt-transport-https wget
 sudo wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt-get update
 sudo apt-get install -y code 
 
 # Install ansible
